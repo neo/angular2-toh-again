b11968868f91ab5f425bd9f56864f6ceb94015e2
Neo <neolwc@gmail.com>
2016-11-11 12:10:10 -0500

    Routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
+  styleUrls: ['app.component.css'],
   template: `
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
   `
 })
 export class AppComponent {
--- app/app.module.ts
+++ app/app.module.ts
@@ -3,17 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }        from './app.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService }         from './hero.service';
 
+import { AppRoutingModule }    from './app-routing.module';
+
 @NgModule({
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
     HeroDetailComponent
   ],
